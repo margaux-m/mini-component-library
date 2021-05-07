a17888c2004f665a67b1fb4cbf0f667ffdb40053
Margaux Mansanarez <margaux.mansanarez@gmail.com>
2021-05-07 11:39:36 +0200

    First attempt Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,38 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <SelectDropdown
+      value={value}
+      onChange={onChange}
+      style={{ 'width': 16 * displayedValue.length + 16 + 'px' }}
+    >
       {children}
-    </select>
+      <Icon id="chevron-down" />
+    </SelectDropdown>
   );
 };
+
+const SelectDropdown = styled.select`
+  font-size: 16px;
+  padding: 12px 16px;
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  border: transparent;
+  background-size: 20px;
+
+  &:focus {
+    outline: 0;
+    box-shadow: 0 0 0 1px blue;
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Wrapper = styled.div`
+
+`;
 
 export default Select;
